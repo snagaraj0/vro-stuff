fec42ff0a72605fe77c5cac8b3e5546cb2708d38
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2019-08-04 19:29:46 -0500

    Update db.py

--- projects/FlaskREST/db.py
+++ projects/FlaskREST/db.py
@@ -9,10 +9,11 @@ c.execute('''CREATE TABLE algo_params(
 algo_id INTEGER PRIMARY KEY AUTOINCREMENT,
 algo_name TEXT NOT NULL,
 algo_param1 TEXT NOT NULL,
-algo_param2 TEXT NOT NULL);''')
-c.execute('''INSERT INTO algo_params (algo_name, algo_param1, algo_param2)
+algo_param2 TEXT NOT NULL,
+algo_param3 TEXT NOT NULL);''')
+c.execute('''INSERT INTO algo_params (algo_name, algo_param1, algo_param2, algo_param3)
     VALUES ('BowtieSpark', '/s1/snagaraj/project_env/SRR639031_1.fastq' , '/s1/snagaraj/project_env/SRR639031_2.fastq');''')
-c.execute('''INSERT INTO algo_params (algo_name, algo_param1, algo_param2)
-    VALUES ('Fragment_assignment', '/s1/snagaraj/project_env/SRR639031_1.fastq_1' , '/s1/snagaraj/project_env/SRR639031_2.fastq_2');''')
+c.execute('''INSERT INTO algo_params (algo_name, algo_param1, algo_param2, algo_param3)
+    VALUES ('Fragment_assignment', '/s1/snagaraj/Homo_sapiens/UCSC/hg19/Sequence/Chromosomes' , 'HindIII', '/s1/snagaraj/project_env/output_alignment.dat');''')
 conn.commit()
 conn.close()
