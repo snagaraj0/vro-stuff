e72edee6057cd872d9a46f3c30bed9227cba08dd
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2019-08-04 19:19:22 -0500

    Update db.py

--- projects/FlaskREST/db.py
+++ projects/FlaskREST/db.py
@@ -3,15 +3,16 @@
 import sqlite3
 
 #populate demo database
-conn = sqlite3.connect("users.db")
+conn = sqlite3.connect("algo_params.db")
 c = conn.cursor()
-c.execute('''CREATE TABLE users(
-user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_name TEXT NOT NULL,
-user_mail TEXT NOT NULL);''')
-c.execute('''INSERT INTO users (user_name, user_mail)
-    VALUES ('Simone Giertz', 'giertz@shittyrobots.loc');''')
-c.execute('''INSERT INTO users (user_name, user_mail)
-    VALUES ('Paula Pinkepank', 'paula@pinkepa.nk');''')
+c.execute('''CREATE TABLE algo_params(
+algo_id INTEGER PRIMARY KEY AUTOINCREMENT,
+algo_name TEXT NOT NULL,
+algo_param1 TEXT NOT NULL,
+algo_param2 TEXT NOT NULL);''')
+c.execute('''INSERT INTO algo_params (algo_name, algo_param1, algo_param2)
+    VALUES ('BowtieSpark', '/s1/snagaraj/project_env/SRR639031_1.fastq' , '/s1/snagaraj/project_env/SRR639031_2.fastq');''')
+c.execute('''INSERT INTO algo_params (algo_name, algo_param1, algo_param2)
+    VALUES ('Fragment_assignment', '/s1/snagaraj/project_env/SRR639031_1.fastq_1' , '/s1/snagaraj/project_env/SRR639031_2.fastq_2');''')
 conn.commit()
 conn.close()
